585004bd3e5fb154bd9799028bd646cf78fa1a8f
zain400 <77384566+zain400@users.noreply.github.com>
2021-04-22 22:57:06 +0500

    Update index.py

---
+++ Assign1/index.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+np.random.seed(1212)
+from sklearn.model_selection import train_test_split
+import keras
+from keras.models import Model
+from keras.layers import *
+from keras import optimizers
