336b3699851cbb2583b10b668d1507dd4d697f39
smusman98 <39401198+smusman98@users.noreply.github.com>
2021-04-22 22:58:43 +0500

    Read csv

--- Assign1/index.py
+++ Assign1/index.py
@@ -7,3 +7,17 @@ import keras
 from keras.models import Model
 from keras.layers import *
 from keras import optimizers
+
+d_f_train = pd.read_csv('train.csv')
+d_f_test = pd.read_csv('test.csv')
+d_f_train.head()
+
+d_f_features = d_f_train.iloc[:, 1:785]
+d_f_label = d_f_train.iloc[:, 0]
+
+X_test = d_f_test.iloc[:, 0:784]
+
+print(X_test.shape)
+X_train, X_cv, y_train, y_cv = train_test_split(d_f_features, d_f_label, 
+                                                test_size = 0.2,
+                                                random_state = 1212)
